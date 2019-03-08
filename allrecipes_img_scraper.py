780b7363a8eeb9f54cd2fea9e78c0e356dc0464d
Richard L Huynh <huynhtastic@gmail.com>
2019-03-08 11:00:47 -0600

    Started modularizing the scraper
    Converted the initialization of the webdriver into a function
    
    Links are now stored for processing instead of printing

--- allrecipes_img_scraper.py
+++ allrecipes_img_scraper.py
@@ -7,30 +7,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-# add webdriver options
-options = webdriver.ChromeOptions()
-options.add_argument(' — incognito')
-#options.add_argument('headless')
+def create_webdriver(incognito:bool=True, headless:bool=True):
+    """Instantiate and return a selenium chrome webdriver.
 
-# initialize chrome instance
-driver_path = os.environ.get('CHROME_DRIVER_PATH')
-if not driver_path:
-    one_time_path = input('No CHROME_DRIVER_PATH environment variable set. '
-                          'Would you like to specify a path to your '
-                          'Chrome driver now? (note: this will not set your '
-                          'environment variable for future runs) [y/N]: ')
-    if one_time_path == 'y' or one_time_path == 'Y':
-        driver_path = input('Please specify the path to your Chrome driver: ')
-    else:
-        raise RuntimeError('No CHROME_DRIVER_PATH environment variable set. '
-                           'Please use `export CHROME_DRIVER_PATH={path to '
-                           'chromedriver file}` in your shell to point to a '
-                           'chrome driver')
-browser = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+    Args:
+        incognito: A flag to set the browser to surf in incognito mode.
+        headless: A flag to tell the driver to not render a visible browser to
+            avoid the resource overhead associated with rendering.
+
+    Returns: a Chrome WebDriver object to use for automated browsing.
+    """
+    # add webdriver options
+    options = webdriver.ChromeOptions()
+    options.add_argument(' — incognito')
+    #options.add_argument('headless')
+
+    # initialize chrome instance
+    driver_path = os.environ.get('CHROME_DRIVER_PATH')
+    if not driver_path:
+        one_time_path = input('No CHROME_DRIVER_PATH environment variable set. '
+                              'Would you like to specify a path to your '
+                              'Chrome driver now? (note: this will not set your '
+                              'environment variable for future runs) [y/N]: ')
+        if one_time_path == 'y' or one_time_path == 'Y':
+            driver_path = input('Please specify the path to your Chrome driver: ')
+        else:
+            raise RuntimeError('No CHROME_DRIVER_PATH environment variable set. '
+                               'Please use `export CHROME_DRIVER_PATH={path to '
+                               'chromedriver file}` in your shell to point to a '
+                               'chrome driver')
+    return webdriver.Chrome(executable_path=driver_path, chrome_options=options)
 
 # define webpage to scrape
 brisket_url = "https://www.allrecipes.com/search/results/?wt=brisket&sort=re&page={}"
-# for testing
+#  for testing
 brisket_url = "https://www.allrecipes.com/search/results/?wt=chicken&sort=re&page={}"
 timeout = 10
 page_num = 1
@@ -46,10 +56,15 @@ except TimeoutException:
     print('Timed out waiting for page to load')
     browser.quit()
 
+# grab the element with the recipe link
+# links = [element.find_element_by_tag_name('a').get_attribute('a')
+#          for element in article_elements]
+links = []
 article_elements = browser.find_elements_by_class_name(recipe_link_class_name)
 for element in article_elements:
     link_holder = element.find_element_by_tag_name('a')
-    print(link_holder.get_attribute('href'))
+    links.append(link_holder.get_attribute('href'))
 
 st()
     #break
+
