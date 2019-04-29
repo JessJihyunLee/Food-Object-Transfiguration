9f18e96b495df4a85e4b5ce8aed0c99e20d2b5f8
Jihyun Lee <jessjihyun.lee@utexas.edu>
2019-04-28 22:46:29 -0500

    Update preprocess_beforerun.py

--- Cycle-GAN/preprocess_beforerun.py
+++ Cycle-GAN/preprocess_beforerun.py
@@ -1,33 +1,34 @@
+# In[1]:
 import zipfile
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 import cv2
 
-#Here I used PaperSpace. I read in scraped images from local machine.
+# In[2]:
+#Here I used PaperSpace Python 3 Jupyter Notebook. I read in scraped images from local machine.
 
 zip_ref = zipfile.ZipFile("./foodcycleGAN.zip",'r')
 zip_ref.extractall()
 zip_ref.close()
-# In[2]:
-os.getcwd()
 
 # In[3]:
+# Codes to check default directory
+os.getcwd()
 os.listdir('./')
 
-# In[6]:
+# In[4]:
 chicken_names = os.listdir('./foodcycleGAN/chicken_wings')
 spring_names = os.listdir('./foodcycleGAN/spring_rolls')
 train_A, test_A, train_B, test_B = train_test_split(chicken_names, spring_names, test_size=0.1, random_state=42)
 
-
-# In[7]:
+# In[5]:
 os.makedirs('./foodcycleGAN/trainA')
 os.makedirs('./foodcycleGAN/trainB')
 os.makedirs('./foodcycleGAN/testA')
 os.makedirs('./foodcycleGAN/testB')
 
-# In[8]:
+# In[6]:
 f = './foodcycleGAN/chicken_wings'
 l = './foodcycleGAN/spring_rolls'
 trna = './foodcycleGAN/trainA'
